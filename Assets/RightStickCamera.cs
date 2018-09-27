708b568dec06065777ac4d614548373f3954af72
Jason Stillwell <dragonfax@gmail.com>
2018-09-27 00:47:31 -0700

    .

---
+++ Assets/RightStickCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class RightStickCamera : UnityStandardAssets.Cameras.PivotBasedCameraRig {
+	protected override void FollowTarget(float deltaTime) {
+		m_Pivot.transform.position = m_Target.transform.position;
+	}
+}
