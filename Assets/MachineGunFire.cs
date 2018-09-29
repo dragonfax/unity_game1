bbc289ef2a157ebd64ad9e2f10ddada99d010769
Jason Stillwell <dragonfax@gmail.com>
2018-09-28 21:55:37 -0700

    .

---
+++ Assets/MachineGunFire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MachineGunFire : MonoBehaviour
+{
+
+    public Rigidbody bullet;
+    public float bulletSpeed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Rigidbody clone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+            clone.velocity = transform.forward * bulletSpeed;
+        }
+    }
+}
---
+++ Assets/SelfDestructDistance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructDistance : MonoBehaviour
+{
+
+    private Vector3 startPosition;
+
+    public float maxDistance;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(startPosition, transform.position) > maxDistance)
+        {
+            Destroy(this);
+        }
+    }
+}
